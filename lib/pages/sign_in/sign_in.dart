d8dee2cc5fabed027df3a52cfaeb28fff3f44f69
julio cesar <jcbarcelosrj@gmail.com>
2026-05-09 19:03:46 -0300

    refactor: add LayoutBuilder

--- lib/pages/sign_in/sign_in.dart
+++ lib/pages/sign_in/sign_in.dart
@@ -11,112 +11,121 @@ class SignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Sign In')),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 20),
-            child: Center(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return Scaffold(
+            appBar: AppBar(title: const Text('Sign In')),
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 100,
+                  horizontal: 20,
                 ),
-                padding: EdgeInsets.all(20),
-                width: MediaQuery.of(context).size.width,
-
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 50,
-                          vertical: 18,
-                        ),
-                        backgroundColor: Colors.blue,
-                        foregroundColor: Colors.white,
-                      ),
-                      onPressed: () {},
-                      child: Text('Entrar com Facebook'),
+                child: Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                    const SizedBox(height: 10),
-                    Column(
+                    padding: EdgeInsets.all(20),
+                    width: constraints.maxWidth < 600 ? 375 : 360,
+
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        SizedBox(
-                          width: double.infinity,
-                          child: Text(
-                            '-------------------------------------ou---------------------------------',
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 50,
+                              vertical: 18,
+                            ),
+                            backgroundColor: Colors.blue,
+                            foregroundColor: Colors.white,
                           ),
-                        ),
-
-                        Text('Acessar com E-mail:'),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    CustomTextFiled(label: 'E-mail'),
-
-                    Observer(
-                      builder: (_) {
-                        return CustomTextFiled(
-                          label: 'Password',
-                          obscureText: signIn.obscuretxt,
-                          suffixIcon: IconButton(
-                            onPressed: () => signIn.setObscuretxt(),
-                            icon: signIn.obscuretxt
-                                ? Icon(Icons.visibility_off)
-                                : Icon(Icons.visibility),
-                          ),
-                        );
-                      },
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
                           onPressed: () {},
-                          child: Text('Esqueceu a senha?'),
+                          child: Text('Entrar com Facebook'),
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Theme.of(context).primaryColor,
-                          foregroundColor: Colors.white,
-                        ),
-                        onPressed: () {},
-                        child: Text('Entrar'),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Divider(),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Não tem uma conta?'),
+                        const SizedBox(height: 10),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: double.infinity,
+                              child: Text(
+                                '-------------------------------------ou---------------------------------',
+                              ),
+                            ),
 
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(builder: (_) => SignupPage()),
+                            Text('Acessar com E-mail:'),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextFiled(label: 'E-mail'),
+
+                        Observer(
+                          builder: (_) {
+                            return CustomTextFiled(
+                              label: 'Password',
+                              obscureText: signIn.obscuretxt,
+                              suffixIcon: IconButton(
+                                onPressed: () => signIn.setObscuretxt(),
+                                icon: signIn.obscuretxt
+                                    ? Icon(Icons.visibility_off)
+                                    : Icon(Icons.visibility),
+                              ),
                             );
                           },
-                          child: Text('Cadastre-se'),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: Text('Esqueceu a senha?'),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).primaryColor,
+                              foregroundColor: Colors.white,
+                            ),
+                            onPressed: () {},
+                            child: Text('Entrar'),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Divider(),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Não tem uma conta?'),
+
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (_) => SignupPage(),
+                                  ),
+                                );
+                              },
+                              child: Text('Cadastre-se'),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
