2d6e608603506e8cdd280d5cfdc41477fa40679f
julio cesar <jcbarcelosrj@gmail.com>
2026-05-09 17:17:30 -0300

    feat: add page login

--- lib/widgets/custom_header_drawer.dart
+++ lib/widgets/custom_header_drawer.dart
@@ -1,30 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:olx_mobx/pages/sign_in/sign_in.dart';
 
 class CustomHeaderDrawer extends StatelessWidget {
   const CustomHeaderDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).primaryColor,
-      height: MediaQuery.of(context).size.height * 0.22,
-      padding: EdgeInsets.only(left: 20, right: 20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.person, color: Colors.white, size: 100),
-          Expanded(
-            child: Column(
-              children: [
-                Text(
-                  'Acesse sua conta agora!',
-                  style: TextStyle(color: Colors.white),
-                ),
-                Text('Clique aqui!', style: TextStyle(color: Colors.white)),
-              ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (_) => SignIn()));
+      },
+      child: Container(
+        color: Theme.of(context).primaryColor,
+        height: MediaQuery.of(context).size.height * 0.22,
+        padding: EdgeInsets.only(left: 20, right: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.person, color: Colors.white, size: 100),
+            Expanded(
+              child: Column(
+                children: [
+                  Text(
+                    'Acesse sua conta agora!',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Text('Clique aqui!', style: TextStyle(color: Colors.white)),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
