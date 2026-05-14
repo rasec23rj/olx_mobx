9f8f4f8e1eaf47dfbb1bd475e7d954471ad30d31
julio cesar <jcbarcelosrj@gmail.com>
2026-05-14 14:10:50 -0300

    feat: add shared_preferences

--- lib/repositories/ibge_repository.dart
+++ lib/repositories/ibge_repository.dart
@@ -1,15 +1,38 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:flutter/rendering.dart';
 import 'package:olx_mobx/models/city_model.dart';
 import 'package:olx_mobx/models/uf_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class IbgeRepository {
   Future<List<UfModel>> getUFListFromApi() async {
+    final preferencs = await SharedPreferences.getInstance();
+
+    if (preferencs.containsKey('UF_LIST')) {
+      debugPrint('FOUND_CACHE');
+      final jsonDecode = json.decode(preferencs.get('UF_LIST').toString());
+      return jsonDecode.map<UfModel>((uf) => UfModel.fromJson(uf)).toList()
+        ..sort(
+          (UfModel a, UfModel b) =>
+              a.name.toLowerCase().compareTo(b.name.toLowerCase()),
+        );
+    }
+
     const endpont =
         'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
     final response = await Dio().get<List>(endpont);
+
     try {
+      preferencs.setString('UF_LIST', json.encode(response.data));
+
+      debugPrint('NOT FOUND_CACHE');
       return response.data!.map<UfModel>((uf) => UfModel.fromJson(uf)).toList()
-        ..sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
+        ..sort(
+          (UfModel a, UfModel b) =>
+              a.name.toLowerCase().compareTo(b.name.toLowerCase()),
+        );
     } on DioException {
       return Future.error('Falha em obter lista de estados');
     }
@@ -24,7 +47,10 @@ class IbgeRepository {
       return response.data!
           .map<CityModel>((city) => CityModel.fromJson(city))
           .toList()
-        ..sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
+        ..sort(
+          (CityModel a, CityModel b) =>
+              a.name.toLowerCase().compareTo(b.name.toLowerCase()),
+        );
     } on DioException {
       return Future.error('Falha em obter lista de cidades');
     }
